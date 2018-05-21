793d26a8da752f08a5bd0b31286d12a3f741d63c
Hristo Vrigazov <hvrigazov@gmail.com>
2018-05-21 21:33:40 +0300

    Initial version of image utils

---
+++ vision-utils/src/main/java/com/vision4j/utils/VisionUtils.java
@@ -0,0 +1,34 @@
+package com.vision4j.utils;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+public class VisionUtils {
+
+    public static BufferedImage resize(BufferedImage img, int width, int height) {
+        BufferedImage resizedImage = new BufferedImage(width, height, img.getType());
+        Graphics2D g = resizedImage.createGraphics();
+        g.drawImage(img, 0, 0, width, height, null);
+        g.setComposite(AlphaComposite.Src);
+
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.dispose();
+
+        return resizedImage;
+    }
+
+    public byte[] getBytes(BufferedImage resizedImage, String formatName) throws IOException {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(resizedImage, formatName, baos);
+        baos.flush();
+        byte[] imageInByte = baos.toByteArray();
+        baos.close();
+        return imageInByte;
+    }
+
+}

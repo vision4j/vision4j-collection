18cef7af54567b237afe4e6f55583fd80ace73e7
Hristo Vrigazov <hvrigazov@gmail.com>
2018-07-04 20:36:55 +0300

    Slightly rework the way we are calling the builder

--- models/grpc-classifier/src/main/java/com/vision4j/classification/GrpcImageNetClassifier.java
+++ models/grpc-classifier/src/main/java/com/vision4j/classification/GrpcImageNetClassifier.java
@@ -1,6 +1,5 @@
 package com.vision4j.classification;
 
-import com.vision4j.utils.ImageSize;
 import io.grpc.ManagedChannel;
 
 import static com.vision4j.utils.Constants.IMAGENET_CATEGORIES;
@@ -10,15 +9,15 @@ import static com.vision4j.utils.Constants.IMAGENET_CATEGORIES;
  * A classifier trained on imagenet called through GRPC
  */
 public class GrpcImageNetClassifier extends GrpcClassifier {
-    public GrpcImageNetClassifier(ImageSize imageSize) {
-        super(IMAGENET_CATEGORIES, imageSize);
+    public GrpcImageNetClassifier() {
+        super(IMAGENET_CATEGORIES);
     }
 
-    public GrpcImageNetClassifier(String host, int port, ImageSize imageSize) {
-        super(host, port, IMAGENET_CATEGORIES, imageSize);
+    public GrpcImageNetClassifier(String host, int port) {
+        super(IMAGENET_CATEGORIES, host, port);
     }
 
-    public GrpcImageNetClassifier(ManagedChannel channel, ImageSize imageSize) {
-        super(channel, IMAGENET_CATEGORIES, imageSize);
+    public GrpcImageNetClassifier(ManagedChannel channel) {
+        super(IMAGENET_CATEGORIES, channel);
     }
 }

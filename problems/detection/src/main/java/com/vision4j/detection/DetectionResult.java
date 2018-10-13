1662c08a6058e3e34228dce9063b1c667d3ca9a5
Hristo Vrigazov <hvrigazov@gmail.com>
2018-10-13 11:43:53 +0300

    Added Maven project for the detection problem

---
+++ problems/detection/src/main/java/com/vision4j/detection/DetectionResult.java
@@ -0,0 +1,39 @@
+package com.vision4j.detection;
+
+import com.vision4j.utils.Category;
+
+import java.util.HashMap;
+
+public class DetectionResult extends HashMap<Category, DetectionResult.BoundingBox> {
+
+    public static class BoundingBox {
+        private int left;
+        private int top;
+        private int right;
+        private int bottom;
+
+        public BoundingBox(int left, int top, int right, int bottom) {
+            this.left = left;
+            this.top = top;
+            this.right = right;
+            this.bottom = bottom;
+        }
+
+        public int getLeft() {
+            return left;
+        }
+
+        public int getTop() {
+            return top;
+        }
+
+        public int getRight() {
+            return right;
+        }
+
+        public int getBottom() {
+            return bottom;
+        }
+    }
+
+}
